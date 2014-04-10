f8b0f113e88ff8dc7b52556d26a652adabcbff40
Florent Solt <florent@solt.biz>
2014-04-10 11:54:14 +0200

    Initial Release

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+module.exports = function(grunt) {
+  var buildPlatforms = parseBuildPlatforms(grunt.option('platforms'));
+
+  grunt.initConfig({
+    nodewebkit: {
+      options: {
+        version: '0.9.2',
+        build_dir: './build',
+        mac_icns: './icon/icon.icns',
+        mac: buildPlatforms.mac,
+        win: buildPlatforms.win,
+        linux32: buildPlatforms.linux32,
+        linux64: buildPlatforms.linux64
+      },
+      src: [
+        './node_modules/**', 
+        '!./node_modules/grunt*/**', 
+        './package.json', 
+        './README.md',
+        './icon/icon.icns'
+      ]
+    },
+    clean: {
+        main: ['build/releases'],
+        cache: ['build/cache']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-node-webkit-builder');
+  grunt.registerTask('default', 'nodewebkit');
+
+};
+
+var parseBuildPlatforms = function(argumentPlatform) {
+  // this will make it build no platform when the platform option is specified
+  // without a value which makes argumentPlatform into a boolean
+  var inputPlatforms = argumentPlatform || process.platform + ";" + process.arch;
+
+  // Do some scrubbing to make it easier to match in the regexes bellow
+  inputPlatforms = inputPlatforms.replace("darwin", "mac");
+  inputPlatforms = inputPlatforms.replace(/;ia|;x|;arm/, "");
+
+  var buildAll = /^all$/.test(inputPlatforms);
+
+  var buildPlatforms = {
+    mac: /mac/.test(inputPlatforms) || buildAll,
+    win: /win/.test(inputPlatforms) || buildAll,
+    linux32: /linux32/.test(inputPlatforms) || buildAll,
+    linux64: /linux64/.test(inputPlatforms) || buildAll
+  };
+
+  return buildPlatforms;
+}
